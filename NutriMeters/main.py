26a146e3ee394b3778f1352ef8944b85a40a927a
Luan Leite <luanleite202@gmail.com>
2025-04-10 17:28:15 -0300

    Add some updates to personal info

---
+++ NutriMeters/main.py
@@ -0,0 +1,154 @@
+from personal import *;
+import os
+## Calcular IMC, recomendar deficit ou superavit calorico 
+## Recomendar alimentação baseando-se na meta pessoal.
+pessoa = Pessoa()
+while True:
+    print("Bem vindo!!\n Antes de começar preciso de algumas informações sobre você.")
+    while True:
+        try:
+            nome = input("Como devo te chamar?: ").capitalize()
+            validarNome(nome)
+            pessoa.nome = nome
+            break
+        except ValueError: 
+            print("Nome invalido. Tente novamente")
+
+    print(f"Ótimo, {pessoa.nome}.")
+
+    while True:
+        sexo = input("Qual o seu sexo?\n 1 - Masculino\n 2 - Feminino\n R: ")
+        teste = gosta(sexo)
+        if teste is None:
+            print("Resposta invalida. Tente novamente") 
+        else:
+            if sexo == "1":
+                pessoa.sexo = "Masculino"
+            else: 
+                pessoa.sexo = "Feminino"
+            break
+
+    while True:
+        try:
+            idade = input("Sua idade: ")
+            validarIdade(idade)
+            pessoa.idade = idade
+            break
+        except ValueError:
+            print("Digite uma idade válida.")
+
+    while True:
+        try:
+            peso = input("Digite agora o seu peso: ")  
+            peso_validado = validarMedidas(peso)
+            if peso_validado is None:
+                print("Peso inválido. Por favor, digite um peso válido.")
+            else:
+                pessoa.peso = float(peso_validado)
+                break 
+        except ValueError:
+            print("Peso inválido. Por favor, digite um peso válido.")
+
+    while True:
+        try:
+            altura = input("Digite agora a sua altura: ")  
+            altura_validada = validarMedidas(altura)
+            if altura_validada is None:
+                print("Altura inválido. Por favor, digite um altura válido.")
+            else:
+                pessoa.altura = float(altura_validada)
+                break 
+        except ValueError:
+            print("Altura inválido. Por favor, digite um peso válido.")
+    print(pessoa.peso, pessoa.altura)
+    pessoa.imc = calcular_imc(float(peso), float(altura))
+
+    while True:
+        meta = define_meta()
+        if meta is None:
+            print("Digite uma meta válida. Tente novamente.")
+        else:
+            if meta == "1":
+                pessoa.meta = "Emagrecer"      
+            else:
+                pessoa.meta = "Engordar"
+            break
+    break  
+
+print("Seu imc:", pessoa.imc)
+tmb = TMB(pessoa)
+print(f"Sua taxa metabolica basal (TMB) é de aproximadamente:{tmb:.2f}")
+
+print(f"\nCerto {pessoa.nome}! Agora preciso de algumas informações sobre sua alimentação:")
+
+info_nutricao = Nutricao()
+while True:
+    while True:
+        verduras_legumes = input("Você gosta de legumes e verduras?\n 1 - SIM\n 2 - NÃO\n R: ")
+        teste = gosta(verduras_legumes)
+        if teste is None:
+            print("Resposta invalida. Tente novamente") 
+        else:
+            info_nutricao.verduras_e_legumes = verduras_legumes
+            break
+
+    while True:
+        frutas = input("Você gosta de frutas?\n 1 - SIM\n 2 - NÃO\n R: ")
+        teste = gosta(frutas)
+        if teste is None:
+            print("Resposta invalida. Tente novamente") 
+        else:
+            info_nutricao.frutas = frutas
+            break
+
+    while True:
+        carne_bovina = input("Você gosta de carne bovina?\n 1 - SIM\n 2 - NÃO\n R: ")
+        teste = gosta(carne_bovina)
+        if teste is None:
+            print("Resposta inválida. Tente novamente") 
+        else:
+            info_nutricao.carne_bovina = carne_bovina
+            break
+
+    while True:
+        carne_branca = input("Você gosta de carne branca(frango, peru, galinha)?\n 1 - SIM\n 2 - NÃO\n R: ")
+        teste = gosta(carne_branca)
+        if teste is None:
+            print("Resposta inválida. Tente novamente") 
+        else:
+            info_nutricao.carne_branca = carne_branca
+            break
+
+    while True:
+        frutos_do_mar = input("Você gosta de frutos do mar?\n 1 - SIM\n 2 - NÃO\n R: ")
+        teste = gosta(frutos_do_mar)
+        if teste is None:
+            print("Resposta inválida. Tente novamente") 
+        else:
+            info_nutricao.frutos_do_mar = frutos_do_mar
+            break
+
+    print("Certo. Estamos quase lá")
+    print("Agora preciso que me informe as coisas que você mais gosta de comer, separados por virgula.")
+
+    while True:
+            preferencias = input('Ex:(Arroz, feijão, carne, farofa...)\nR: ').replace(", ", ",")
+            if not all(item.isalpha() for item in preferencias.split(",")):
+                print("Digite apenas letras.")
+            else:
+                info_nutricao.preferencias = preferencias.split(",")
+                print("Certo.")
+                break
+
+    print("Agora preciso que me informe as coisas que você não gosta, separados por virgula")
+    while True:
+        exclusoes = input("Ex:(Carne de porco, brocolis...)\nR: ").replace(", ", ",")
+        if not all(item.isalpha() for item in exclusoes.split(",")):
+                print("Digite apenas letras.")
+        else:
+            info_nutricao.exclusao = exclusoes.split(",")
+            print("Certo.")
+            break
+    break
+
+
---
+++ NutriMeters/personal.py
@@ -0,0 +1,105 @@
+# Dados para Analise:
+
+# Dados pessoais:
+class Pessoa:
+    def __init__(self):
+        self.nome = ""
+        self.sexo = ""
+        self.idade = 0
+        self.peso = 0
+        self.altura = 0
+        self.imc = 0
+        self.meta = ""
+        self.rotina = ""
+
+    def __str__(self):
+        return f"Nome: {self.nome}\nSexo: {self.sexo}\nIdade: {self.idade}\nPeso: {self.peso} kg\nAltura: {self.altura} m\nIMC: {self.imc:.2f}\nMeta: {self.meta}"
+
+# Definindo IMC = peso / (altura x altura)
+# imc < 18.5 = Magreza
+# imc >= 18.5 and imc <= 24.9 = Normal
+# imc > 24.9 and imc <= 29.9 = Sobrepeso
+# imc > 29.9 and imc <= 39.9 = Obesidade
+# imc > 40.0 = Obesidade grave
+
+def calcular_imc(x: float, y: float) -> float:
+    # A fórmula do IMC é: peso / (altura * altura)
+    return x / (y ** 2)
+
+def validarNome(nome):
+    nome = nome.strip()  # Remove espaços extras
+    if not nome.isalpha():  # Verifica se o nome contém apenas letras
+        raise ValueError("O nome deve conter apenas letras e não pode ser vazio.")
+    return nome  
+    
+    
+def validarIdade(idade):
+    idade = idade.strip()
+    idades_invalidas = [0, ""]
+    if int(idade) and idade not in idades_invalidas:
+        return idade 
+    
+def validarMedidas(medida):
+    medida = medida.strip()  # Remove leading/trailing spaces
+    medidas_invalidas = [0, ""]
+
+    try:
+        medida_float = float(medida)
+    except ValueError:
+        return None
+
+    if medida_float not in medidas_invalidas and medida:
+        return medida
+    else:
+        return None
+    
+def define_meta():
+    meta = input("Qual a sua meta?\n 1 Emagrecer\n2 Engordar\n")
+    values = ['1', '2']
+    if meta in values:
+        return meta
+    else:
+        return None
+    
+
+# Dados nutricionais
+class Nutricao:
+    def __init__(self):
+        self.verduras_e_legumes = True
+        self.frutas = True
+        self.carne_bovina = True
+        self.carne_branca = True
+        self.frutos_do_mar = True
+        self.preferencias = []
+        self.exclusao = []
+
+def gosta(meta):
+    values = ['1', '2']
+    if meta in values:
+        return meta
+    else:
+        return None
+
+# Informações consideraveis
+
+# Calcular a taxa metabolica basal:
+# 66 + (13,8 x peso em kg) + (5 x altura em cm) - (6,8 x idade em anos)
+
+def TMB(pessoa):
+    if pessoa.sexo == 'masculino':
+        return 88.36 + (13.4 * pessoa.peso) + (4.8 * pessoa.altura) - (5.7 * int(pessoa.idade))
+    else:
+        return 447.6 + (9.2 * pessoa.peso) + (3.1 * pessoa.altura) - (4.3 * int(pessoa.idade))
+    
+# Verificar atividade fisica:
+
+#Sedentário – Pouco ou nenhum exercício diário: Multiplique sua TMB por 1,20;
+# Levemente Ativo – (Exercício leve/1 a 3 dias na semana): Multiplique sua TMB por 1,37;
+# Moderadamente Ativo – (Exercício moderado/ 3 a 5 dias na semana): Multiplique sua TMB por 1,55;
+# Bastante Ativo – (Exercício pesado 6 a 7 dias na semana): Multiplique sua TMB por 1,72;
+# Muito Ativo (Exercício pesado todos dias da semana ou treinos 2x ao dia): Multiplique sua TMB por 1,90.
+
+def rotina(tmb, exe):
+    match exe:
+        case "Sedentário":
+            return tmb * 1.20
